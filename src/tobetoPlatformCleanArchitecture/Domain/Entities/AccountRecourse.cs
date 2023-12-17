04fa21ab4bc983430200abac8e6db35a0fba0e53
OzgurSonmez <ozgursonmez00@gmail.com>
2023-12-17 14:35:51 +0300

    Recourse entities added

--- src/tobetoPlatformCleanArchitecture/Domain/Entities/AccountRecourse.cs
+++ src/tobetoPlatformCleanArchitecture/Domain/Entities/AccountRecourse.cs
@@ -6,19 +6,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Domain.Entities;
-public class AccountApplication : Entity<int>
+public class AccountRecourse : Entity<int>
 {
     public int AccountId { get; set; }
     public int ApplicationId { get; set; }
     public int ApplicationStepId { get; set; }
 
-    public AccountApplication()
+    public AccountRecourse()
     {
         
     }
 
-    public AccountApplication(int accountId, int applicationId, int applicationStepId) : this()
+    public AccountRecourse(int id, int accountId, int applicationId, int applicationStepId) : this()
     {
+        Id = id;
         AccountId = accountId;
         ApplicationId = applicationId;
         ApplicationStepId = applicationStepId;
