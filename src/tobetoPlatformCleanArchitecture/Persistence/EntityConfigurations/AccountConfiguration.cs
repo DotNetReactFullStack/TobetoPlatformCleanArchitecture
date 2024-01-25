fae564579e1083eb4815b35f55278a5225908462
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2024-01-25 21:41:54 +0300

    Updated AccountConfiguration.cs

--- src/tobetoPlatformCleanArchitecture/Persistence/EntityConfigurations/AccountConfiguration.cs
+++ src/tobetoPlatformCleanArchitecture/Persistence/EntityConfigurations/AccountConfiguration.cs
@@ -11,7 +11,6 @@ public class AccountConfiguration : IEntityTypeConfiguration<Account>
         builder.ToTable("Accounts").HasKey(a => a.Id);
 
         builder.Property(a => a.Id).HasColumnName("Id").IsRequired();
-        builder.Property(a => a.AddressId).HasColumnName("AddressId");
         builder.Property(a => a.NationalIdentificationNumber).HasColumnName("NationalIdentificationNumber");
         builder.Property(a => a.BirthDate).HasColumnName("BirthDate");
         builder.Property(a => a.PhoneNumber).HasColumnName("PhoneNumber");
