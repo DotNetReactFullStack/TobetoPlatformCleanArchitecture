fdd471ba57abc6452441a9ceacfda511dda810b6
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2024-01-26 23:43:13 +0300

    Added ContractType, Contract and AccountContract entities

---
+++ src/tobetoPlatformCleanArchitecture/Domain/Entities/AccountContract.cs
@@ -0,0 +1,30 @@
+﻿using Core.Persistence.Repositories;
+using System;
+namespace Domain.Entities
+{
+    public class AccountContract : Entity<int>
+    {
+        public int AccountId { get; set; }
+        public int ContractId { get; set; }
+        public bool IsAccept { get; set; }
+
+        public Account? Account { get; set; }
+        public Contract? Contract { get; set; }
+
+        public AccountContract()
+        {
+
+        }
+
+        public AccountContract(int id, int accountId, int contractId, bool isAccept, Account? account, Contract? contract) : this()
+        {
+            Id = id;
+            AccountId = accountId;
+            ContractId = contractId;
+            IsAccept = isAccept;
+            Account = account;
+            Contract = contract;
+        }
+    }
+}
+
