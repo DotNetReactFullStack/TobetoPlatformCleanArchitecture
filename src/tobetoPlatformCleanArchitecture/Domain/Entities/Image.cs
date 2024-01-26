536dc165bc4db69d37d3abd7c302ff6f28b5f68c
=burhan-xD <=burhanozsahinoglu@windowslive.com>
2024-01-26 19:37:12 +0300

    Added Image and ImageExtension entities

---
+++ src/tobetoPlatformCleanArchitecture/Domain/Entities/Image.cs
@@ -0,0 +1,31 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+public class Image : Entity<int>
+{
+    public int ImageExtensionId { get; set; }
+    public string Name { get; set; }
+    public string Url { get; set; }
+    public bool IsActive { get; set; }
+
+    public virtual ImageExtension ImageExtension { get; set; }
+    public Image()
+    {
+
+    }
+
+    public Image(int id, int imageExtensionId, string name, string url, bool isActive) : this()
+    {
+        Id = id;
+        ImageExtensionId = imageExtensionId;
+        Name = name;
+        Url = url;
+        IsActive = isActive;
+    }
+}
+
