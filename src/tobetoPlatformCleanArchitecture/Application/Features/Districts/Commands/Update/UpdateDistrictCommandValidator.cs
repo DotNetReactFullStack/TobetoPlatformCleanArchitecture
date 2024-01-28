a16b33baed92a10d3196107d6a6a178b1dae9c1b
OzgurSonmez <ozgursonmez00@gmail.com>
2024-01-28 19:42:56 +0300

    Added validation rules for District entity's create and update operations

--- src/tobetoPlatformCleanArchitecture/Application/Features/Districts/Commands/Update/UpdateDistrictCommandValidator.cs
+++ src/tobetoPlatformCleanArchitecture/Application/Features/Districts/Commands/Update/UpdateDistrictCommandValidator.cs
@@ -8,7 +8,7 @@ public class UpdateDistrictCommandValidator : AbstractValidator<UpdateDistrictCo
     {
         RuleFor(c => c.Id).NotEmpty();
         RuleFor(c => c.CityId).NotEmpty();
-        RuleFor(c => c.Name).NotEmpty();
+        RuleFor(c => c.Name).NotEmpty().MaximumLength(30);
         RuleFor(c => c.Priority).NotEmpty();
         RuleFor(c => c.Visibility).NotEmpty();
     }
