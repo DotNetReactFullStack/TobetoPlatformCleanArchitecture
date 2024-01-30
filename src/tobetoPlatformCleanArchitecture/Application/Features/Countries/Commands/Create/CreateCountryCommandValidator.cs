87103a641378e56ebea812674cec84a2174bd7f3
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2024-01-30 20:09:07 +0300

    Refactoring Country command validators

--- src/tobetoPlatformCleanArchitecture/Application/Features/Countries/Commands/Create/CreateCountryCommandValidator.cs
+++ src/tobetoPlatformCleanArchitecture/Application/Features/Countries/Commands/Create/CreateCountryCommandValidator.cs
@@ -9,8 +9,7 @@ public class CreateCountryCommandValidator : AbstractValidator<CreateCountryComm
         RuleFor(c => c.Name).NotEmpty();
         RuleFor(c => c.Priority).NotEmpty();
         RuleFor(c => c.Visibility).NotEmpty();
-
-        RuleFor(c => c.Name).MinimumLength(2).MaximumLength(30);
-
+        RuleFor(c => c.Name).MinimumLength(2);
+        RuleFor(c => c.Name).MaximumLength(30);
     }
 }
--- src/tobetoPlatformCleanArchitecture/Application/Features/Countries/Commands/Update/UpdateCountryCommandValidator.cs
+++ src/tobetoPlatformCleanArchitecture/Application/Features/Countries/Commands/Update/UpdateCountryCommandValidator.cs
@@ -10,8 +10,7 @@ public class UpdateCountryCommandValidator : AbstractValidator<UpdateCountryComm
         RuleFor(c => c.Name).NotEmpty();
         RuleFor(c => c.Priority).NotEmpty();
         RuleFor(c => c.Visibility).NotEmpty();
-
-        RuleFor(c => c.Name).MinimumLength(2).MaximumLength(30);
-
+        RuleFor(c => c.Name).MinimumLength(2);
+        RuleFor(c => c.Name).MaximumLength(30);
     }
 }
