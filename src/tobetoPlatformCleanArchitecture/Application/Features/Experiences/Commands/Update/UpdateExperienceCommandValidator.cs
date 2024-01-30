8d8fc8e2009de8f6f6faab0e0f5fb0a12b74d0ea
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2024-01-30 20:12:02 +0300

    Updated Experience command validators

--- src/tobetoPlatformCleanArchitecture/Application/Features/Experiences/Commands/Update/UpdateExperienceCommandValidator.cs
+++ src/tobetoPlatformCleanArchitecture/Application/Features/Experiences/Commands/Update/UpdateExperienceCommandValidator.cs
@@ -9,13 +9,15 @@ public class UpdateExperienceCommandValidator : AbstractValidator<UpdateExperien
         RuleFor(c => c.Id).NotEmpty();
         RuleFor(c => c.AccountId).NotEmpty();
         RuleFor(c => c.CityId).NotEmpty();
-        RuleFor(c => c.CompanyName).NotEmpty().MaximumLength(30);
-        RuleFor(c => c.JobTitle).NotEmpty().MaximumLength(40);
-        RuleFor(c => c.Industry).NotEmpty().MaximumLength(30);
+        RuleFor(c => c.CompanyName).NotEmpty();
+        RuleFor(c => c.CompanyName).MaximumLength(30);
+        RuleFor(c => c.JobTitle).NotEmpty();
+        RuleFor(c => c.JobTitle).MaximumLength(40);
+        RuleFor(c => c.Industry).NotEmpty();
+        RuleFor(c => c.Industry).MaximumLength(30);
         RuleFor(c => c.StartingDate).NotEmpty();
-        //RuleFor(c => c.EndingDate).NotEmpty();
         RuleFor(c => c.IsCurrentlyWorking).NotEmpty();
-        RuleFor(c => c.Description).MaximumLength(1000);
+        RuleFor(c => c.Description).MaximumLength(300);
         RuleFor(c => c.IsActive).NotEmpty();
     }
 }
