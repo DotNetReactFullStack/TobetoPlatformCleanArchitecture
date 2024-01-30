e89b64adc6b365cb9838cc034aa58920f80eb848
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2024-01-31 01:24:18 +0300

    Updated UpdateAccountCommandValidator

--- src/tobetoPlatformCleanArchitecture/Application/Features/Accounts/Commands/Update/UpdateAccountCommandValidator.cs
+++ src/tobetoPlatformCleanArchitecture/Application/Features/Accounts/Commands/Update/UpdateAccountCommandValidator.cs
@@ -8,16 +8,12 @@ public class UpdateAccountCommandValidator : AbstractValidator<UpdateAccountComm
     public UpdateAccountCommandValidator()
     {
         RuleFor(c => c.Id).NotEmpty();
-        RuleFor(c => c.UserId).NotEmpty();
-        RuleFor(c => c.NationalIdentificationNumber).NotEmpty();
         RuleFor(c => c.BirthDate).NotEmpty();
         RuleFor(c => c.PhoneNumber).NotEmpty();
         //RuleFor(c => c.ProfilePhotoPath).NotEmpty();
         RuleFor(c => c.ShareProfile).NotEmpty();
         RuleFor(c => c.ProfileLinkUrl).NotEmpty();
         RuleFor(c => c.IsActive).NotEmpty();
-
         RuleFor(a => a.PhoneNumber).Length(12).Matches(new Regex(@"((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}"));
-        RuleFor(a => a.NationalIdentificationNumber).Length(11).Matches(new Regex(@"^[1-9]{1}[0-9]{9}[02468]{1}$"));
     }
 }
