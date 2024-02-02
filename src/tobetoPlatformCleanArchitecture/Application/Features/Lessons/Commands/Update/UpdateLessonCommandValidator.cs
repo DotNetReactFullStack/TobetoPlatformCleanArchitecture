ff9d5a3a7819173a3b2dee4ec6fe8d8901d73804
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2024-02-02 16:43:03 +0300

    Updated Lessons create and update commands' validator

--- src/tobetoPlatformCleanArchitecture/Application/Features/Lessons/Commands/Update/UpdateLessonCommandValidator.cs
+++ src/tobetoPlatformCleanArchitecture/Application/Features/Lessons/Commands/Update/UpdateLessonCommandValidator.cs
@@ -9,10 +9,10 @@ public class UpdateLessonCommandValidator : AbstractValidator<UpdateLessonComman
         RuleFor(c => c.Id).NotEmpty();
         RuleFor(c => c.CourseId).NotEmpty();
         RuleFor(c => c.Name).NotEmpty().MaximumLength(50);
-        RuleFor(c => c.Visibility).NotEmpty();
+        RuleFor(c => c.Visibility).NotNull();
         RuleFor(c => c.Language).NotEmpty().MaximumLength(30);
         RuleFor(c => c.Content).NotEmpty().MaximumLength(300);
         RuleFor(c => c.Duration).NotEmpty().GreaterThanOrEqualTo(0);
-        RuleFor(c => c.IsActive).NotEmpty();
+        RuleFor(c => c.IsActive).NotNull();
     }
 }
