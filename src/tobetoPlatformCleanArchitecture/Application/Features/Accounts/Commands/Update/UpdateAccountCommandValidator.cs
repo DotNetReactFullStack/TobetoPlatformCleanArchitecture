a7c3b84c8cd98a24c0e379cd0e4037d2121ddf4d
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2024-02-05 23:11:15 +0300

    UpdateAccountCommand: Added a property to check the current user and implemented a business rule

--- src/tobetoPlatformCleanArchitecture/Application/Features/Accounts/Commands/Update/UpdateAccountCommandValidator.cs
+++ src/tobetoPlatformCleanArchitecture/Application/Features/Accounts/Commands/Update/UpdateAccountCommandValidator.cs
@@ -8,7 +8,6 @@ public class UpdateAccountCommandValidator : AbstractValidator<UpdateAccountComm
     public UpdateAccountCommandValidator()
     {
         RuleFor(c => c.Id).NotEmpty();
-        //RuleFor(c => c.NationalIdentificationNumber).NotEmpty(); // Uncomment for NationalIdentificationNumber to be updateable
         RuleFor(c => c.AboutMe).NotEmpty();
         RuleFor(c => c.BirthDate).NotEmpty();
         RuleFor(c => c.PhoneNumber).NotEmpty();
@@ -16,7 +15,6 @@ public class UpdateAccountCommandValidator : AbstractValidator<UpdateAccountComm
         RuleFor(c => c.ProfileLinkUrl).NotEmpty();
         RuleFor(c => c.IsActive).NotNull();
 
-        //RuleFor(a => a.NationalIdentificationNumber).Length(11).Matches(new Regex(@"^[1-9]{1}[0-9]{9}[02468]{1}$")); // Uncomment for NationalIdentificationNumber to be updateable
         RuleFor(a => a.PhoneNumber).Length(12).Matches(new Regex(@"((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}"));
     }
 }
