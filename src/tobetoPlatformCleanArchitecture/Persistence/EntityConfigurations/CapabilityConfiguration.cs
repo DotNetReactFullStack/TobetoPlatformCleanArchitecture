bb371d2771dbcaeac11817d8354f4f96eb97be75
Ersin Kaya <79023498+ersin-kaya@users.noreply.github.com>
2024-02-15 12:41:12 +0300

    Added seed data for Capabilities

--- src/tobetoPlatformCleanArchitecture/Persistence/EntityConfigurations/CapabilityConfiguration.cs
+++ src/tobetoPlatformCleanArchitecture/Persistence/EntityConfigurations/CapabilityConfiguration.cs
@@ -19,5 +19,40 @@ public class CapabilityConfiguration : IEntityTypeConfiguration<Capability>
         builder.Property(c => c.DeletedDate).HasColumnName("DeletedDate");
 
         builder.HasQueryFilter(c => !c.DeletedDate.HasValue);
+
+        builder.HasData(getSeeds());
+    }
+
+    private HashSet<Capability> getSeeds()
+    {
+        int id = 0;
+        HashSet<Capability> seeds =
+            new()
+            {
+                new Capability { Id = ++id, Name = "SOLID", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = "AOP", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = "OOP", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = "TypeScript", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = "JavaScript", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = "Vue.js", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = "React", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = "Angular", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = "Vue", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = ".NET", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = "C#", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = "Spring Boot", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = "Java", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = "J2EE", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = "C++", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = "C", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = "STM32", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = "CSS", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = "Tailwind CSS", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = "Bootstrap", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = "EF Core", Priority= 1, Visibility=true },
+                new Capability { Id = ++id, Name = "SQL", Priority= 1, Visibility=true },
+            };
+
+        return seeds;
     }
 }
