7cd58bc684dfd09451e656dcf6523e031f92268d
=burhan-xD <=burhanozsahinoglu@windowslive.com>
2024-02-26 00:56:30 +0300

    charcterTestv02

--- src/tobetoPlatformCleanArchitecture/Persistence/EntityConfigurations/CollegeConfiguration.cs
+++ src/tobetoPlatformCleanArchitecture/Persistence/EntityConfigurations/CollegeConfiguration.cs
@@ -38,7 +38,7 @@ public class CollegeConfiguration : IEntityTypeConfiguration<College>
                 new College { Id = ++id, Name = "Hacettepe Üniversitesi", Visibility=true },
                 new College { Id = ++id, Name = "İstanbul Üniversitesi", Visibility=true },
                 new College { Id = ++id, Name = "Gazi Üniversitesi", Visibility=true },
-                //new College { Id = ++id, Name = "Gazi Üniversitesi", Visibility=true },
+                //new College { Id = ++id, Name = "Gazi Üniversitesi  ıİ çş ÇÇ ŞŞ Şö üĞ ğ ", Visibility=true },
             };
         return seeds;
     }
